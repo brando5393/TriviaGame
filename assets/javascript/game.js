31e11dda5872b378beb8cc769ec199c055856a38
Brandon Williams <brando5393@gmail.com>
2019-12-30 19:22:09 -0600

    started working on js

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+// global vars
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var startBtnClicked = false;
+// timer var
+
+//display vars
+var timerDisplayText = $("#timer-display");
+var correctAnswersDisplayText = $("#correct-answer-display");
+var incorrectAnswersDisplayText = $("#incorrect-answer-display");
+
+// document ready
+$(document).ready(function(){
+
+    // create a timer set for 1 minute
+    
+    // user clicks start button timer begins to count down and questions display
+    
+    // if timer reaches 0 or user clicks submit button timer stops and the number of correct vs incorrect answers are displayed. Also, questions become hidden again
+})
