11e78d2b4e74f8d7cbf886b44a477e4b1ccce687
Brandon Williams <brando5393@gmail.com>
2020-01-06 18:25:50 -0600

    fixed unreadable text and grouped form choices togeather with name attr

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,17 +4,27 @@ var incorrectAnswers = 0;
 var startBtnClicked = false;
 // timer var
 
-//display vars
+// display vars
 var timerDisplayText = $("#timer-display");
 var correctAnswersDisplayText = $("#correct-answer-display");
 var incorrectAnswersDisplayText = $("#incorrect-answer-display");
 
+// audio vars
+var mainAudio = $("#main-audio");
+
+$("#trivia-questions").hide();
+
 // document ready
-$(document).ready(function(){
+$(document).ready(function () {
+
+    $(".start-btn").click(function () {
+        $("#trivia-questions").show();
+        startBtnClicked = true;
+    })
 
     // create a timer set for 1 minute
-    
+
     // user clicks start button timer begins to count down and questions display
-    
+
     // if timer reaches 0 or user clicks submit button timer stops and the number of correct vs incorrect answers are displayed. Also, questions become hidden again
 })
