e71ed24ccf49a9b309559dde3e7583378548a4f7
Brandon Williams <brando5393@gmail.com>
2020-01-08 16:57:14 -0600

    added logic for timer

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,66 @@
 // global vars
 var correctAnswers = 0;
 var incorrectAnswers = 0;
-var startBtnClicked = false;
+var mainTimerRunning = false;
 // timer var
 
 // display vars
 var timerDisplayText = $("#timer-display");
 var correctAnswersDisplayText = $("#correct-answer-display");
 var incorrectAnswersDisplayText = $("#incorrect-answer-display");
+
+// timeout var
+var mainTimeout;
+
+// score game function
+function scoreGame(){
+
+}
+
+// timer obj
+var mainTimer = {
+    time: 90,
+    
+    start: function(){
+        if(!mainTimerRunning){
+            $("#trivia-questions").show();
+            timerDisplayText.text("1:30");
+            $("#welcome-audio").trigger('play');
+            $("#main-audio").trigger('play');
+            mainTimeout = setTimeout(function(){
+                mainTimerRunning = true;
+            },90000)
+        }
+    },
+
+    count: function(){
+        mainTimer.time--;
+        var convertedTime = mainTimer.timeConverter(mainTimer.time);
+        timerDisplayText.text(convertedTime);
+    },
+
+    timeConverter: function(t){
+        var minutes = Math.floor(t / 60);
+        var seconds = t - (minutes * 60);
+        if(seconds < 10){
+            seconds = "0" + seconds;
+        }
+    },
+
+    stop: function(){
+        clearTimeout(mainTimeout);
+        mainTimerRunning = false;
+        $("#main-audio").trigger('pause');
+        $("#end-game-audio").trigger('play');
+    },
+
+    outOfTime: function(){
+        clearTimeout(mainTimeout);
+        mainTimerRunning = false;
+        $("#main-audio").trigger('pause');
+        $("#magic-word-audio").trigger('play');
+    }
+}
 
 
 
@@ -18,33 +71,19 @@ $(document).ready(function () {
 
 
     $(".start-btn").click(function () {
-        $("#trivia-questions").show();
-        startBtnClicked = true;
-        timerDisplayText.text("1:30");
-        $("#welcome-audio").trigger('play');
-        $("#main-audio").trigger('play');
+       mainTimer.start();
+       mainTimer.count();
     })
 
 
 
     $(".submit-btn").click(function(){
-        startBtnClicked = false;
-        $("#main-audio").trigger('pause');
-        $("#end-game-audio").trigger('play');
+        mainTimer.stop();
     })
-
-    // create a timer set for 1 and 1/2 minutes
-    var mainTimer = setTimeout(function(){
-        console.log("timer working");
-    },90000);
 
     // user clicks start button timer begins to count down and questions display
 
     // if timer reaches 0 or user clicks submit button timer stops and the number of correct vs incorrect answers are displayed. Also, questions become hidden again
-    if(mainTimer === 0 || submitBtnClicked === true){
-        clearTimeout(mainTimer);
-        scoreGame();
-    }
 })
 
 
