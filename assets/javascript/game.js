0fb2acd8929547ee76b969b669b59a99b0a276ce
Brandon Williams <brando5393@gmail.com>
2020-01-08 12:18:29 -0600

    got audio working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,22 +9,38 @@ var timerDisplayText = $("#timer-display");
 var correctAnswersDisplayText = $("#correct-answer-display");
 var incorrectAnswersDisplayText = $("#incorrect-answer-display");
 
-// audio vars
-var mainAudio = $("#main-audio");
+
 
 $("#trivia-questions").hide();
 
 // document ready
 $(document).ready(function () {
 
+
     $(".start-btn").click(function () {
         $("#trivia-questions").show();
         startBtnClicked = true;
+        timerDisplayText.text("1:30");
+        $("#main-audio").trigger('play')
     })
 
-    // create a timer set for 1 minute
+    // create a timer set for 1 and 1/2 minutes
+    var mainTimer = setTimeout(function(){
+        console.log("timer working");
+    },90000);
 
     // user clicks start button timer begins to count down and questions display
 
     // if timer reaches 0 or user clicks submit button timer stops and the number of correct vs incorrect answers are displayed. Also, questions become hidden again
+    if(mainTimer === 0 || submitBtnClicked === true){
+        clearTimeout(mainTimer);
+        scoreGame();
+    }
 })
+
+
+
+//create array for correct answers
+//create array for user answers
+//set question input values to a number. if value number is equal to user answer correct else incorrect
+//reread docs on every method in bootcamp folder
